1023cf294e5f7874ea2e9cc592ba4633c375f20d
Jason Xu <jasonxu@Jasons-MacBook-Pro.local>
2023-05-02 23:41:14 -0400

    we back

--- src/App.js
+++ src/App.js
@@ -1,42 +1,58 @@
-import React, { Component } from 'react';
-import './App.css';
-import {
-  HashRouter,
-  Route
-} from "react-router-dom";
-import Everything from './pages/0everything';
-import Blog from './pages/5Blog.js';
-import OneFiveSix from './pages/6OneFiveSix.js';
-import AthleteBrandingComparison from './articles/1AthleteBrandingComparison.js'
-import GroceryShopping from './articles/2GroceryShopping.js'
-import ChineseVirus from './articles/3ChineseVirus.js'
-import SpotifySentiment from './articles/4SpotifySentiment.js'
-import SpotifyGenre from './articles/5SpotifyGenre.js'
-import BubblePredictions1 from './articles/6BubblePredictions1.js'
-import BubblePredictions2 from './articles/7BubblePredictions2.js'
+import React, { Component } from "react";
+import "./App.css";
+import { HashRouter, Route } from "react-router-dom";
+import Everything from "./pages/0everything";
+import Blog from "./pages/5Blog.js";
+import OneFiveSix from "./pages/6OneFiveSix.js";
+import AthleteBrandingComparison from "./articles/1AthleteBrandingComparison.js";
+import GroceryShopping from "./articles/2GroceryShopping.js";
+import ChineseVirus from "./articles/3ChineseVirus.js";
+import SpotifySentiment from "./articles/4SpotifySentiment.js";
+import SpotifyGenre from "./articles/5SpotifyGenre.js";
+import BubblePredictions1 from "./articles/6BubblePredictions1.js";
+import BubblePredictions2 from "./articles/7BubblePredictions2.js";
 
 class App extends Component {
-  componentDidMount(){
+  componentDidMount() {
     document.title = "Jason Xu";
   }
   render() {
     return (
       <HashRouter basename="/" hashType="slash">
-        
         <div>
           <Route exact path="/" component={Everything} />
           <Route exact path="/Blog" component={Blog} />
           <Route exact path="/OneFiveSix" component={OneFiveSix} />
-          <Route exact path="/Blog/Athlete-Branding-Comparison" component={AthleteBrandingComparison} />
-          <Route exact path="/Blog/Grocery-Shopping" component={GroceryShopping} />
+          <Route
+            exact
+            path="/Blog/Athlete-Branding-Comparison"
+            component={AthleteBrandingComparison}
+          />
+          <Route
+            exact
+            path="/Blog/Grocery-Shopping"
+            component={GroceryShopping}
+          />
           <Route exact path="/Blog/Chinese-Virus" component={ChineseVirus} />
-          <Route exact path="/Blog/Spotify-Sentiment" component={SpotifySentiment} />
+          <Route
+            exact
+            path="/Blog/Spotify-Sentiment"
+            component={SpotifySentiment}
+          />
           <Route exact path="/Blog/Spotify-Genre" component={SpotifyGenre} />
-          <Route exact path="/Blog/Bubble-Predictions-Rockets" component={BubblePredictions1} />
-          <Route exact path="/Blog/Bubble-Predictions-Suns" component={BubblePredictions2} />
+          <Route
+            exact
+            path="/Blog/Bubble-Predictions-Rockets"
+            component={BubblePredictions1}
+          />
+          <Route
+            exact
+            path="/Blog/Bubble-Predictions-Suns"
+            component={BubblePredictions2}
+          />
         </div>
       </HashRouter>
-    )
+    );
   }
 }
 
--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,66 +1,167 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import { Link } from 'react-router-dom';
-import styles from './Header.module.css';
-import Scroll from 'react-scroll'
+import styles from "./Header.module.css";
+import Scroll from "react-scroll";
 import { Link, animateScroll as scroll } from "react-scroll";
-import { Link as RedirectLink} from 'react-router-dom';
+import { Link as RedirectLink } from "react-router-dom";
 var ScrollLink = Scroll.ScrollLink;
 
-
-
-
-
-
-
 class Header extends Component {
+  componentDidMount() {}
 
-  componentDidMount() {
-    
-  }
-
-  render(){
-    return(
-      <div style={{width:'100%'}}>
-        {this.props.pageNum === 0 && <div className={styles.header}>
-          <Link to="about_element" spy={true} smooth={true} duration={500} className={styles.header_text} activeClass={styles.header_text_active}>JASON XU</Link>
-          <div className = {styles.menu}>
-            <Link id={styles.to_hide} to="experience_element" spy={true} offset={-75} smooth={true} duration={500} className={styles.header_text} activeClass={styles.header_text_active}>EXPERIENCE</Link>
-            <Link id={styles.to_hide} to="education_element" spy={true} offset={-75} smooth={true} duration={500} className={styles.header_text} activeClass={styles.header_text_active}>EDUCATION</Link>
-            <Link id={styles.to_hide} to="skills_element" spy={true} offset={-75} smooth={true} duration={500} className={styles.header_text} activeClass={styles.header_text_active}>SKILLS</Link>
-            <Link id={styles.to_hide} to="projects_element" spy={true} offset={-75} smooth={true} duration={500} className={styles.header_text} activeClass={styles.header_text_active}>PROJECTS</Link>
-            
-            <RedirectLink to="/blog" className={styles.header_text}>BLOG</RedirectLink>
-            <RedirectLink to="/onefivesix" className={styles.header_text}>156</RedirectLink>
+  render() {
+    return (
+      <div style={{ width: "100%" }}>
+        {this.props.pageNum === 0 && (
+          <div className={styles.header}>
+            <Link
+              to="about_element"
+              spy={true}
+              smooth={true}
+              duration={500}
+              className={styles.header_text}
+              activeClass={styles.header_text_active}
+            >
+              JASON XU
+            </Link>
+            <div className={styles.menu}>
+              <Link
+                id={styles.to_hide}
+                to="experience_element"
+                spy={true}
+                offset={-75}
+                smooth={true}
+                duration={500}
+                className={styles.header_text}
+                activeClass={styles.header_text_active}
+              >
+                EXPERIENCE
+              </Link>
+              <Link
+                id={styles.to_hide}
+                to="education_element"
+                spy={true}
+                offset={-75}
+                smooth={true}
+                duration={500}
+                className={styles.header_text}
+                activeClass={styles.header_text_active}
+              >
+                EDUCATION
+              </Link>
+              <Link
+                id={styles.to_hide}
+                to="skills_element"
+                spy={true}
+                offset={-75}
+                smooth={true}
+                duration={500}
+                className={styles.header_text}
+                activeClass={styles.header_text_active}
+              >
+                SKILLS
+              </Link>
+              <Link
+                id={styles.to_hide}
+                to="projects_element"
+                spy={true}
+                offset={-75}
+                smooth={true}
+                duration={500}
+                className={styles.header_text}
+                activeClass={styles.header_text_active}
+              >
+                PROJECTS
+              </Link>
+              <RedirectLink to="/onefivesix" className={styles.header_text}>
+                156
+              </RedirectLink>
+            </div>
           </div>
-        </div>}
+        )}
 
-        {this.props.pageNum === 4 && <div className={styles.header}>
-          <RedirectLink to="/" className={styles.header_text}>JASON XU</RedirectLink>
-          <div className = {styles.menu}>
-            <RedirectLink id={styles.to_hide} to="/" className={styles.header_text}>EXPERIENCE</RedirectLink>
-            <RedirectLink id={styles.to_hide} to="/" className={styles.header_text}>EDUCATION</RedirectLink>
-            <RedirectLink id={styles.to_hide} to="/" className={styles.header_text}>SKILLS</RedirectLink>
-            <RedirectLink id={styles.to_hide} to="/" className={styles.header_text}>PROJECTS</RedirectLink>
-            
-            <RedirectLink to="/blog" className={styles.blog_selected}>BLOG</RedirectLink>
-            <RedirectLink to="/onefivesix" className={styles.header_text}>156</RedirectLink>
+        {this.props.pageNum === 4 && (
+          <div className={styles.header}>
+            <RedirectLink to="/" className={styles.header_text}>
+              JASON XU
+            </RedirectLink>
+            <div className={styles.menu}>
+              <RedirectLink
+                id={styles.to_hide}
+                to="/"
+                className={styles.header_text}
+              >
+                EXPERIENCE
+              </RedirectLink>
+              <RedirectLink
+                id={styles.to_hide}
+                to="/"
+                className={styles.header_text}
+              >
+                EDUCATION
+              </RedirectLink>
+              <RedirectLink
+                id={styles.to_hide}
+                to="/"
+                className={styles.header_text}
+              >
+                SKILLS
+              </RedirectLink>
+              <RedirectLink
+                id={styles.to_hide}
+                to="/"
+                className={styles.header_text}
+              >
+                PROJECTS
+              </RedirectLink>
+              <RedirectLink to="/onefivesix" className={styles.header_text}>
+                156
+              </RedirectLink>
+            </div>
           </div>
-        </div>}
-        {this.props.pageNum === 5 && <div className={styles.header}>
-          <RedirectLink to="/" className={styles.header_text}>JASON XU</RedirectLink>
-          <div className = {styles.menu}>
-            <RedirectLink id={styles.to_hide} to="/" className={styles.header_text}>EXPERIENCE</RedirectLink>
-            <RedirectLink id={styles.to_hide} to="/" className={styles.header_text}>EDUCATION</RedirectLink>
-            <RedirectLink id={styles.to_hide} to="/" className={styles.header_text}>SKILLS</RedirectLink>
-            <RedirectLink id={styles.to_hide} to="/" className={styles.header_text}>PROJECTS</RedirectLink>
-            
-            <RedirectLink to="/blog" className={styles.header_text}>BLOG</RedirectLink>
-            <RedirectLink to="/onefivesix" className={styles.blog_selected}>156</RedirectLink>
+        )}
+        {this.props.pageNum === 5 && (
+          <div className={styles.header}>
+            <RedirectLink to="/" className={styles.header_text}>
+              JASON XU
+            </RedirectLink>
+            <div className={styles.menu}>
+              <RedirectLink
+                id={styles.to_hide}
+                to="/"
+                className={styles.header_text}
+              >
+                EXPERIENCE
+              </RedirectLink>
+              <RedirectLink
+                id={styles.to_hide}
+                to="/"
+                className={styles.header_text}
+              >
+                EDUCATION
+              </RedirectLink>
+              <RedirectLink
+                id={styles.to_hide}
+                to="/"
+                className={styles.header_text}
+              >
+                SKILLS
+              </RedirectLink>
+              <RedirectLink
+                id={styles.to_hide}
+                to="/"
+                className={styles.header_text}
+              >
+                PROJECTS
+              </RedirectLink>
+              <RedirectLink to="/onefivesix" className={styles.blog_selected}>
+                156
+              </RedirectLink>
+            </div>
           </div>
-        </div>}
-
+        )}
       </div>
-    )
+    );
   }
 }
 
